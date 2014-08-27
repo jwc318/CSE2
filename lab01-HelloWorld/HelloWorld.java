737b92ac0435df1f9a1ff488270aba1d0dbcdbe8
Joey Carney <jwc318@lehigh.edu>
2014-08-27 16:57:15 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+/////////////////////
+// My name is Joey Carney
+// Demo 0
+// Hello World Java Program
+//
+
+
+
+
+//create a public class named HelloWorld
+public class HelloWorld{
+    //create a main method that will hold the print statements
+    public static void main(String[] args) {
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("#                       #");
+    System.out.println("#  Hello World 2014!!   #");
+    System.out.println("#                       #");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("test");
+    //test the program
+        }
+    }
